2bed92ad6c4cd07542c66e8324cf73b2ac12bf08
Martha <marthamergia@yahoo.com>
2021-08-10 22:02:40 -0400

    commit

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ const quizContainer = document.getElementsByClassName('quiz')
 const resultContainer = document.getElementsByClassName('results')
 const submitButton = document.getElementsByClassName('submit')
 
-function buildQuiz() {}
-function showResult() {}
-
-//display quiz on page load
-buildQuiz()
-
-//on submit, show results
-submitButton.addvEventListner('click', showResult)
 
 const myQuestions = [
     {
@@ -115,19 +107,53 @@ const myQuestions = [
 
 ]
 
-function buildQuiz() {
-    const output = []
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-            const answers = []
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
-            for(letter in currentQuestion.answers) {
-                answers.push(
-                    <lable>
-                    
-                    </lable>
-                )
-            }
-        }
-    )
+	function showQuestions(questions, quizContainer){
+		// we'll need a place to store the output and the answer choices
+	var output = [];
+	var answers;
+
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
+
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			// ...add an html radio button
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		// add this question and its answers to the output
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
+}
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		// code will go here
+	}
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
 }
